c4baeff9ff872e0ffb9f456fdd0b2d2607824734
Tenzin Dekyong <dhontenzin@gmail.com>
2022-04-06 19:25:49 +0200

    styles changed

--- inc/header.php
+++ inc/header.php
@@ -8,17 +8,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/styles.css">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Recipes</title>
 </head>
 <body>
 <!-- main container -->
-    <div class="container-fluid">
-        <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Home</a>
-        </div>
+    <div class="">
+        <nav class="navbar navbar-expand-lg navbar-dark border" id="navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
         </nav>
--- index.php
+++ index.php
@@ -1,51 +1,30 @@
 <?php include './inc/header.php' ?>
 
         <!-- card start -->
-        <div class="card mx-auto mt-2" id="card" >
-            <?php
-                $result = $recipes->getAllRecipes();
-                if($result) {
-                    $img = [];
-                    while($rows = $result->fetch_assoc()) {
-                        if(strpos($rows['image'], "@type")) {
-                            $image = json_decode($rows['image']);
-                            $image = $image->url;
-                            $img[] = $image;
-                        }else {
-                            $img[] = $rows['image'];
-                        }
-                    }
-            ?>
-                        <img src="<?= $img[rand(0,count($img)-1)] ?>" class="card-img-top" id="card-image"  alt="...">
-            <?php
-                    }
-            ?>
-                
-            <div class="card-body">
-                <h5 class="card-title text-center display-5">Search for Recipe <i class="fas fa-utensils"></i></h5>
-                <!-- form input -->
-                <form method="POST" action="recipeLists.php">
-                    <div class="mb-3" id="input-ingredient">
-                        <input name="ingredient" type="text" placeholder="Enter an ingredient" class="form-control px-5" />
-                        <i class="fas fa-search" id="fa-magnifying-glass"></i>
-                    </div>
+            <div class="card mx-auto my-5 border p-5" id="card" >
+                <div class="card-body">
+                    <h5 class="card-title text-center display-5">Search for Recipe <i class="fas fa-utensils"></i></h5>
 
-                    <div class="range-slider">
-                        <label for="customRange2" class="form-label mx-3  text-secondary"><i class="fas fa-hourglass-start"></i>&nbsp;&nbsp;&nbsp;Total time</label><output class="display-6">35</output>&nbsp;mins
-                        <input name="range-input" type="range" class="form-range" min="10" max="60" step="5" oninput="this.previousElementSibling.value = this.value">
-                        <div class="d-flex justify-content-between">
-                            <i class="fas fa-skiing text-secondary">&nbsp;10 min ...</i>
-                            <i class="fas fa-walking text-secondary">&nbsp;... 60mins</i>
+                    <!-- form start -->
+                    <form method="POST" action="recipeLists.php">
+                        <div class="mb-3" id="input-ingredient">
+                            <input name="ingredient" type="text" placeholder="Enter an ingredient" class="form-control px-5" />
+                            <i class="fas fa-search" id="fa-magnifying-glass"></i>
                         </div>
-                    </div>
 
-                    <input type="submit" value="Submit" name="submit" class="btn btn-outline-primary w-100 mt-4">
-                </form>
-
-                <!-- form ends -->
-
+                        <div class="range-slider">
+                            <label for="customRange2" class="form-label mx-3  text-secondary"><i class="fas fa-hourglass-start"></i>&nbsp;&nbsp;&nbsp;Total time</label><output class="display-6">35</output>&nbsp;mins
+                            <input name="range-input" type="range" class="form-range" min="10" max="60" step="5" oninput="this.previousElementSibling.value = this.value">
+                            <div class="d-flex justify-content-between">
+                                <i class="fas fa-skiing text-secondary">&nbsp;10 min ...</i>
+                                <i class="fas fa-walking text-secondary">&nbsp;... 60mins</i>
+                            </div>
+                        </div>
+                        <input type="submit" value="Submit" name="submit" class="btn btn-outline-primary w-100 mt-4">
+                    </form>
+                    <!-- form ends -->
+                </div>
             </div>
-        </div>
         <!-- end of card -->
     </div>
     <!-- end of main container -->
