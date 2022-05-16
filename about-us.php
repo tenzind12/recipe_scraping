29d109300edb4716281e5fd3fb90aa065d370b18
Tenzin Dekyong <dhontenzin@gmail.com>
2022-05-16 15:18:22 +0200

    deleted unwanted pics

--- about-us.php
+++ about-us.php
@@ -6,7 +6,7 @@
             <h2 class="text-grey">About us</h2>
             <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-6" id="about-us__image_container">
             <img src="./assets/images/about-us.png" alt="cartoon group employees" style="max-width: 500px; ">
         </div>
     </div>
