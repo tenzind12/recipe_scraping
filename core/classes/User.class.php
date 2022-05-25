f65df57591b7cd37d3baa339164e0dae39d1efa8
Tenzin Dekyong <dhontenzin@gmail.com>
2022-05-25 12:15:24 +0200

    minor code formatting

--- core/classes/User.class.php
+++ core/classes/User.class.php
@@ -1,28 +1,31 @@
 <?php
-    include_once __DIR__.'/../connection/Db.php';
-    include_once __DIR__.'/../helpers/HelperClass.class.php';
+include_once __DIR__ . '/../connection/Db.php';
+include_once __DIR__ . '/../helpers/HelperClass.class.php';
 
 
-class User {
+class User
+{
     private $db;
     private $class_helper;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new ConnectionDB();
         $this->class_helper = new HelperClass();
     }
 
     // ========== R E G I S T E R   U S E R ================ //
-    public function register($name, $email, $password, $country, $file) {
-        if(empty($name) ||empty($email) || empty($password) || empty($country)) {
-            $msg = $this->class_helper->alertMessage('danger','Sign up failed !', 'Only image field is optional');
+    public function register($name, $email, $password, $country, $file)
+    {
+        if (empty($name) || empty($email) || empty($password) || empty($country)) {
+            $msg = $this->class_helper->alertMessage('danger', 'Sign up failed !', 'Only image field is optional');
             return $msg;
         }
 
         $emailQuery = "SELECT email FROM users WHERE email = '$email'";
         $emailResult = $this->db->query($emailQuery);
-        if($emailResult != null) {
-            $msg = $this->class_helper->alertMessage('danger','Sign up failed !', 'Email already exists');
+        if ($emailResult != null) {
+            $msg = $this->class_helper->alertMessage('danger', 'Sign up failed !', 'Email already exists');
             return $msg;
         }
 
@@ -42,12 +45,12 @@ class User {
         $allowed_images = ['png', 'jpg', 'jpeg', 'gif', 'webp'];
 
         // handle image if exist
-        if($image_size > 0) {
-            if(!in_array($image_extention, $allowed_images)) {
-                $msg = $this->class_helper->alertMessage('danger','Image not supported', 'Supported files: png, jpg, jpeg, gif, webp');
+        if ($image_size > 0) {
+            if (!in_array($image_extention, $allowed_images)) {
+                $msg = $this->class_helper->alertMessage('danger', 'Image not supported', 'Supported files: png, jpg, jpeg, gif, webp');
                 return $msg;
             }
-            
+
             // assign new name
             $image_new_name = substr(md5(time()), 0, 10) . '.' . $image_extention;
             $image_new_location = 'assets/images/users/' . $image_new_name;
@@ -56,10 +59,10 @@ class User {
             $query = "INSERT INTO users ( `name`, `email`, `password`, `image`, `country`) VALUES ('$name','$email','$password','$image_new_name','$country')";
             $result = $this->db->insert($query);
 
-            if($result) {
+            if ($result) {
                 $msg = $this->class_helper->alertMessage('success', 'Sign up Success !', 'You can now log in.');
                 return $msg;
-            }else {
+            } else {
                 $msg = $this->class_helper->alertMessage('danger', 'Sign up failed !', 'Please try again');
                 return $msg;
             }
@@ -67,10 +70,10 @@ class User {
             $query = "INSERT INTO users ( `name`, `email`, `password`, `image`, `country`) VALUES ('$name','$email','$password', null ,'$country')";
             $result = $this->db->insert($query);
 
-            if($result) {
+            if ($result) {
                 $msg = $this->class_helper->alertMessage('success', 'Sign up Success !', 'You can now log in.');
                 return $msg;
-            }else {
+            } else {
                 $msg = $this->class_helper->alertMessage('danger', 'Sign up failed !', 'Please try again');
                 return $msg;
             }
@@ -78,21 +81,22 @@ class User {
     }
 
     // ================ L O G I N   U S E R ================ //
-    public function login($email, $password) {
-        if(empty($email) || empty($password)) {
+    public function login($email, $password)
+    {
+        if (empty($email) || empty($password)) {
             $msg = $this->class_helper->alertMessage('danger', 'Empty field !', 'Please fill all fields');
             return $msg;
         }
 
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
             $msg = $this->class_helper->alertMessage('danger', 'Email error !', 'Email format is not supported');
             return $msg;
         }
 
-        
+
         $query = "SELECT * FROM users WHERE email = '$email' AND password = md5('$password')";
         $result = $this->db->query($query);
-        if($result) {
+        if ($result) {
             $user_data = $result->fetch_assoc();
             Session::set('userLogin', true);
             Session::set('userId', $user_data['id']);
@@ -108,12 +112,13 @@ class User {
     }
 
     // ================ U P D A T E   U S E R ============== //
-    public function update_user($data, $file, $userId) {
+    public function update_user($data, $file, $userId)
+    {
         $userName = $data['userName'];
         $email = $data['email'];
         $country = $data['country'];
 
-        if(empty($userName) || empty($email) || empty($country)) {
+        if (empty($userName) || empty($email) || empty($country)) {
             $msg = $this->class_helper->alertMessage('danger', 'Empty field !', 'All fields must be filled');
             return $msg;
         }
@@ -131,19 +136,19 @@ class User {
         $allowed_images = ['png', 'jpg', 'jpeg', 'gif', 'webp'];
 
         // IF NEW IMAGE 
-        if($image_size > 0) {
+        if ($image_size > 0) {
             // image unlink form folder
             $image_query = "SELECT * from users where id = '$userId'";
             $image_result = $this->db->query($image_query);
             // unlink if image is not null
-            if($image_result)
-                while($rows = $image_result->fetch_assoc())
-                    if($rows['image'] != null)
-                        unlink('./assets/images/users/'.$rows['image']);
+            if ($image_result)
+                while ($rows = $image_result->fetch_assoc())
+                    if ($rows['image'] != null)
+                        unlink('./assets/images/users/' . $rows['image']);
 
 
-            if(!in_array($image_extention, $allowed_images)) {
-                $msg = $this->class_helper->alertMessage('danger','Image not supported', 'Supported files: png, jpg, jpeg, gif, webp');
+            if (!in_array($image_extention, $allowed_images)) {
+                $msg = $this->class_helper->alertMessage('danger', 'Image not supported', 'Supported files: png, jpg, jpeg, gif, webp');
                 return $msg;
             }
 
@@ -161,11 +166,11 @@ class User {
             $updateUser = $this->db->insert($query);
 
 
-            if($updateUser) {
+            if ($updateUser) {
                 Session::set('userPhoto', $image_new_name);
                 return true;
             } else {
-                $msg = $this->class_helper->alertMessage('danger','Update failed !', 'Something went wrong.');
+                $msg = $this->class_helper->alertMessage('danger', 'Update failed !', 'Something went wrong.');
                 return $msg;
             }
         }
@@ -178,24 +183,25 @@ class User {
                     WHERE id='$userId' ";
         $updateUser = $this->db->insert($query);
 
-        if ($updateUser) return true; 
+        if ($updateUser) return true;
         else {
-            $msg = $this->class_helper->alertMessage('danger','Update failed !', 'Something went wrong.');
+            $msg = $this->class_helper->alertMessage('danger', 'Update failed !', 'Something went wrong.');
             return $msg;
         }
     }
 
     // ================ D E L E T E   U S E R ================ //
-    public function delete_user($userId) {
+    public function delete_user($userId)
+    {
         // image delete query
         $img_query = "SELECT image FROM users WHERE id='$userId'";
         $img_result = $this->db->query($img_query);
-        unlink('./assets/images/users/'.$img_result->fetch_assoc()['image']);
+        unlink('./assets/images/users/' . $img_result->fetch_assoc()['image']);
 
         // user delete query
         $query = "DELETE FROM users WHERE id='$userId'";
         $delete_result = $this->db->insert($query);
-        if($delete_result) Session::destroy();
+        if ($delete_result) Session::destroy();
         else echo 'somehting went wrong';
     }
 
@@ -203,41 +209,42 @@ class User {
      *  +++++++++++++++++++ ADMIN PART  +++++++++++++++
      */
     // ================ G E T  A L L  U S E R ================ //
-    public function get_all_users() {
+    public function get_all_users()
+    {
         $query = "SELECT * FROM users";
         $result = $this->db->query($query);
         return $result;
     }
 
     // ============= ADMIN D E L E T E S  USER =============== //
-    public function delete_user_by_admin($userId) {
+    public function delete_user_by_admin($userId)
+    {
         $check_user_query = "SELECT * FROM users WHERE id ='$userId'";
         $user_exist = $this->db->query($check_user_query);
-        if($user_exist) {
+        if ($user_exist) {
             // image delete query
             $img_query = "SELECT image FROM users WHERE id='$userId'";
             $img_result = $this->db->query($img_query);
             $img_name = $img_result->fetch_assoc()['image'];
-            if($img_name) unlink('../assets/images/users/'.$img_name);
-    
+            if ($img_name) unlink('../assets/images/users/' . $img_name);
+
             // user delete query
             $query = "DELETE FROM users WHERE id='$userId'";
             $delete_result = $this->db->insert($query);
 
-            if($delete_result) {
+            if ($delete_result) {
                 unset($_SESSION['userLogin']);
                 unset($_SESSION['userId']);
                 unset($_SESSION['userName']);
                 unset($_SESSION['userPhoto']);
                 unset($_SESSION['userEmail']);
                 unset($_SESSION['userCountry']);
-                $msg = $this->class_helper->alertMessage('success','Success!', 'User has been deleted');
+                $msg = $this->class_helper->alertMessage('success', 'Success!', 'User has been deleted');
                 return $msg;
             } else {
-                $msg = $this->class_helper->alertMessage('danger','Failed !', 'User not deleted. Please try again');
+                $msg = $this->class_helper->alertMessage('danger', 'Failed !', 'User not deleted. Please try again');
                 return $msg;
             }
         }
     }
-
 }
--- core/connection/Db.php
+++ core/connection/Db.php
@@ -1,8 +1,9 @@
-<?php include_once __DIR__.'/../connection/config.php'; ?>
+<?php include_once __DIR__ . '/../connection/config.php'; ?>
 
-<?php 
+<?php
 
-class ConnectionDB {
+class ConnectionDB
+{
     private $host = HOST;
     private $username = USERNAME;
     private $password = PASSWORD;
@@ -10,39 +11,41 @@ class ConnectionDB {
 
     private $con;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->connect();
     }
 
     // connection to database
-    public function connect () {
+    public function connect()
+    {
         $this->con = new mysqli($this->host, $this->username, $this->password, $this->db_name);
-        if(mysqli_connect_errno()) {
-            $this->error = "Connectiont failed ". mysqli_connect_errno();
+        if (mysqli_connect_errno()) {
+            $this->error = "Connectiont failed " . mysqli_connect_errno();
             return false;
         }
     }
 
     // S E L E C T
-    public function query($query) {
+    public function query($query)
+    {
         $result = $this->con->query($query);
-        if($result) {
-            if($result->num_rows > 0) return $result;
-        }
-        else return false;
+        if ($result) {
+            if ($result->num_rows > 0) return $result;
+        } else return false;
     }
 
     // I N S E R T 
-    public function insert($query) {
+    public function insert($query)
+    {
         $result = $this->con->query($query);
         return $result;
     }
 
     // D E L E T E
-    public function delete($query) {
+    public function delete($query)
+    {
         $result = $this->con->query($query);
         return $result;
     }
-
-
 }
